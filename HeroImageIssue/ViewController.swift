3a7c76a9d3d9e3cab1cfb989f65e5835c5983148
Logan Anderson <poggus@gmail.com>
2020-05-22 12:22:46 +0800

    Initial Commit

---
+++ HeroImageIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeroImageIssue
+//
+//  Created by Logan Anderson on 5/22/20.
+//  Copyright © 2020 Logan Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
