0650db537cd2717f78e64b16c9a9f880990452c9
Logan Anderson <poggus@gmail.com>
2020-05-23 08:32:56 +0800

    Initial commit

--- HeroImageIssue/ViewController.swift
+++ HeroImageIssue/ViewController.swift
@@ -6,15 +6,36 @@
 //  Copyright © 2020 Logan Anderson. All rights reserved.
 //
 
+import Hero
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var textFieldA: UITextField!
+    @IBOutlet weak var textFieldB: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        guard let nextVc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "viewController") as? ViewController else {
+            return
+        }
+        navigationController?.hero.isEnabled = true
+        nextVc.hero.isEnabled = true
+        self.hero.isEnabled = true
+        navigationController?.heroNavigationAnimationType = .autoReverse(presenting: .slide(direction: .left))
+        _ = nextVc.view
+        
+        
+        bgImageView.hero.id = "bgImageView"
+        nextVc.bgImageView.hero.id = "bgImageView"
 
+        navigationController?.pushViewController(nextVc, animated: true)
+    }
+    
 }
 
